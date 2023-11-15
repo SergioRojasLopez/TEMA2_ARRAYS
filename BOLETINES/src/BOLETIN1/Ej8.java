8d14afe459fabad3faf2644615f471d5691311b1
SergioRojasLopez <sergiorojaslopez12@gmail.com>
2023-11-15 15:27:08 +0100

    Boletin 1 Arrays - 7

--- BOLETINES/src/BOLETIN1/Ej8.java
+++ BOLETINES/src/BOLETIN1/Ej8.java
@@ -1,20 +1,16 @@
 package BOLETIN1;
 
-import javax.sound.midi.Soundbank;
-import java.util.Scanner;
-
 public class Ej8 {
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
 
         int[] Array = {2, 6, -4, 78, 32, -43};
         int elementoBuscar = -4;
 
-        int posicion = encontrarPosicion (Array,elementoBuscar);
+        int posicion = encontrarPosicion(Array, elementoBuscar);
 
-        if (posicion != -1){
+        if (posicion != -1) {
 
             System.out.println("El valor " + elementoBuscar + " se encuentra en la posicion " + posicion);
         } else {
